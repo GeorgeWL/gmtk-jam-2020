98eaf1b812d1da7209d3f2df7c18b9b1492dfa98
GeorgeWL <georgelangham1990@gmail.com>
2020-07-12 01:42:51 +0100

    cleanup code. add tsconfig missing keys

--- src/controllers/getRandomKeybind.ts
+++ src/controllers/getRandomKeybind.ts
@@ -5,13 +5,9 @@ import getRandomArrayItem from "../helpers/getRandomArrayItem";
 const ALLOWED_KEYBINDS = 'abcdefghijklmnopqrstuvwxyz';
 
 export default (keyReleased: EnumKeyBinds, currentConfig: IKeyMap) => {
-  // check what currentConfig is and store keys in array/set,
   const currentKeys = [currentConfig.up, currentConfig.left, currentConfig.right]
-  // split allowed keybinds to array, remove those those in currentKeys
   const keysAllowedArray = ALLOWED_KEYBINDS.split('').filter(key => currentKeys.includes(key))
-  // get a random key from the allowed array
   const newKey = getRandomArrayItem(keysAllowedArray);
-  // return new config
   const newConfig = { ...currentConfig, [EnumKeyBinds[keyReleased]]: newKey }
   return newConfig
 }
--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,3 @@
 if (module.hot) {
   module.hot.accept()
 }
-console.log('JAVASCRIPT')
-const testElement: HTMLElement | null = document.getElementById('test')
-if (testElement) {
-  testElement.innerText = 'Testing'
-  setTimeout(() => {
-    testElement.innerText = 'Testing Timeout'
-  }, 2000);
-}
