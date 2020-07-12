c86e41ac3962ed0b808f87ed68315856926d1619
GeorgeWL <georgelangham1990@gmail.com>
2020-07-12 17:47:52 +0100

    init keybind disaply befor get lunch

---
+++ src/scenes/modules/keybindDisplay.ts
@@ -0,0 +1,14 @@
+import 'phaser'
+export default class KeybindDisplay {
+  private keybinds
+
+  constructor(keybinds) {
+
+  }
+  createKeybindDisplay(keybinds) {
+    return keybinds
+  }
+  setHighlightedKeybind(key) {
+
+  }
+}
