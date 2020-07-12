44cb8fef25af1b3b34530bd1dbe653ee4a8535b0
GeorgeWL <georgelangham1990@gmail.com>
2020-07-12 01:21:06 +0100

    webpack can begone. parcel is new god

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,12 @@
 // Hello World
+if (module.hot) {
+  module.hot.accept()
+}
 console.log('JAVASCRIPT')
+const testElement: HTMLElement | null = document.getElementById('test')
+if (testElement) {
+  testElement.innerText = 'Testing'
+  setTimeout(() => {
+    testElement.innerText = 'Testing Timeout'
+  }, 2000);
+}
