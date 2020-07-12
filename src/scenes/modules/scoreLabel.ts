979bd1f6662915e250447b1e9bd69aab487cf407
GeorgeWL <georgelangham1990@gmail.com>
2020-07-12 17:46:14 +0100

    score label create from guide. bomb spawner from guide

---
+++ src/scenes/modules/scoreLabel.ts
@@ -0,0 +1,24 @@
+import 'phaser';
+const formatScore = (score: number) => `Score: ${score}`
+
+export default class ScoreLabel extends Phaser.GameObjects.Text {
+  private score: number = 0;
+  constructor(scene: Phaser.Scene, x: number, y: number, score: number, style: Phaser.Types.GameObjects.Text.TextStyle) {
+    super(scene, x, y, formatScore(score), style)
+
+    this.score = score
+  }
+
+  setScore(score: number) {
+    this.score = score
+    this.updateScoreText()
+  }
+
+  add(points: number) {
+    this.setScore(this.score + points)
+  }
+
+  updateScoreText() {
+    this.setText(formatScore(this.score))
+  }
+}
